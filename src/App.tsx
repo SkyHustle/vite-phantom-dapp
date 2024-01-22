e3575f51e49bd607ea3188de2c698c63244fe8ea
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-22 14:09:00 -0700

    Wire up phantom connect button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useWeb3React } from "@web3-react/core";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { connector, hooks } = useWeb3React();
 
   return (
     <>
@@ -17,19 +18,14 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="App">
+        <h1>Web3-React Connector Playbox</h1>
+        <div className="card">
+          <Card connector={connector} hooks={hooks} name="phantom" />
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
