86e266701a7ac224309c9f48e69827eeec228fd4
Dmitry Sky <dmitry.sky@icloud.com>
2024-01-22 13:47:30 -0700

    Wrap our Vite app in this <Web3ReactProvider> JSX component which will give us global access to the hooks that we need to interact with Phantom.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Web3ReactProvider, Web3ReactHooks } from "@web3-react/core";
+import { Connector } from "@web3-react/types";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import allConnections from "./connectors";
+
+const connections: [Connector, Web3ReactHooks][] = allConnections.map(([connector, hooks]) => [connector, hooks]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Web3ReactProvider connectors={connections}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Web3ReactProvider>
 );
